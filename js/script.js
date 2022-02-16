bf0740cac08a7a658aac8df3e15932f3224df51b
mk-hasan <mkhasan6891@gmail.com>
2022-02-17 00:45:23 +0600

    balance amount calculation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// input field
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputAmount = inputField.value;
@@ -9,17 +10,17 @@ function getInputValue(inputId) {
 
 document.getElementById('calcu-button').addEventListener
     ('click', function () {
+        // cost calculation
         const foodCostValue = getInputValue('food-cost');
         const rentCostValue = getInputValue('rent-cost');
         const clothCostValue = getInputValue('cloth-cost');
-
+        // total exp
         const totalExp = document.getElementById('total-exp');
         totalExp.innerText = foodCostValue + rentCostValue + clothCostValue;
-
-
-        const incomeInput = document.getElementById('income-field');
-        const incomeAmount = incomeInput.value;
-        const balanceInput = document.getElementById('balance-after-exp');
-        const balanceAmount = balanceInput.innerText;
-        balanceInput.innerText = incomeAmount - totalExp.innerText;
+        // income
+        const incomeAmountValue = getInputValue('income-field')
+        // balance after expenses
+        const balanceTotal = document.getElementById('balance-after-exp');
+        const balanceAmount = balanceTotal.innerText;
+        balanceTotal.innerText = incomeAmountValue - totalExp.innerText;
     })
