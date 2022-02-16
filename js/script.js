39463f7a833d34a9a2cd566ba212cdad4ec12f03
mk-hasan <mkhasan6891@gmail.com>
2022-02-16 23:05:55 +0600

    js script added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 document.getElementById('calcu-button').addEventListener
     ('click', function () {
-        console.log('calc clicked');
+        const foodInput = document.getElementById('food-cost');
+        const foodCost = foodInput.value;
+        const rentInput = document.getElementById('rent-cost');
+        const rentCost = rentInput.value;
+        const clothInput = document.getElementById('cloth-cost');
+        const clothCost = clothInput.value;
+        const totalExp = document.getElementById('total-exp');
+        totalExp.innerText = parseInt(foodInput.value) + parseInt(rentInput.value) + parseInt(clothInput.value);
+        const incomeInput = document.getElementById('income-field');
+        const incomeAmount = incomeInput.value;
     })
