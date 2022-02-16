ad3217e7e798288f86aee68a83f0ad0f544563b3
mk-hasan <mkhasan6891@gmail.com>
2022-02-16 23:53:47 +0600

    getinputvalue function added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmount = inputField.value;
+    const inputAmountValue = parseFloat(inputAmount);
+    // clear expenses field area 
+    inputField.value = '';
+    return inputAmountValue;
+}
+
 document.getElementById('calcu-button').addEventListener
     ('click', function () {
-        const foodInput = document.getElementById('food-cost');
-        const foodCost = foodInput.value;
-        const rentInput = document.getElementById('rent-cost');
-        const rentCost = rentInput.value;
-        const clothInput = document.getElementById('cloth-cost');
-        const clothCost = clothInput.value;
+        const foodCostValue = getInputValue('food-cost');
+        const rentCostValue = getInputValue('rent-cost');
+        const clothCostValue = getInputValue('cloth-cost');
+
         const totalExp = document.getElementById('total-exp');
-        totalExp.innerText = parseInt(foodInput.value) + parseInt(rentInput.value) + parseInt(clothInput.value);
+        totalExp.innerText = foodCostValue + rentCostValue + clothCostValue;
+
+
         const incomeInput = document.getElementById('income-field');
         const incomeAmount = incomeInput.value;
+        const balanceInput = document.getElementById('balance-after-exp');
+        const balanceAmount = balanceInput.innerText;
+        balanceInput.innerText = incomeAmount - totalExp.innerText;
     })
