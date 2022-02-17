a6f36f048981865d6dc3d54b021512979feae88f
mk-hasan <mkhasan6891@gmail.com>
2022-02-17 09:02:03 +0600

    saving percent added

--- js/script.js
+++ js/script.js
@@ -2,25 +2,43 @@
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputAmount = inputField.value;
-    const inputAmountValue = parseFloat(inputAmount);
-    // clear expenses field area 
-    inputField.value = '';
-    return inputAmountValue;
+    if (isNaN(inputField.value) == true) {
+        alert('pls give number')
+    } else {
+        const inputAmountValue = parseFloat(inputAmount);
+        // clear expenses field area 
+        inputField.value = '';
+        return inputAmountValue;
+    }
+
 }
 
-document.getElementById('calcu-button').addEventListener
+document.getElementById('calcu-btn').addEventListener
     ('click', function () {
         // cost calculation
-        const foodCostValue = getInputValue('food-cost');
-        const rentCostValue = getInputValue('rent-cost');
-        const clothCostValue = getInputValue('cloth-cost');
+        const foodCostValue = getInputValue('food-cost', false);
+        const rentCostValue = getInputValue('rent-cost', false);
+        const clothCostValue = getInputValue('cloth-cost', false);
         // total exp
         const totalExp = document.getElementById('total-exp');
         totalExp.innerText = foodCostValue + rentCostValue + clothCostValue;
         // income
-        const incomeAmountValue = getInputValue('income-field')
+        const incomeAmountValue = getInputValue('income-field', false)
         // balance after expenses
         const balanceTotal = document.getElementById('balance-after-exp');
         const balanceAmount = balanceTotal.innerText;
         balanceTotal.innerText = incomeAmountValue - totalExp.innerText;
+
+    })
+
+document.getElementById('save-btn').addEventListener
+    ('click', function () {
+        const incomeInputText = document.getElementById('income-field');
+        let incomeAmount = parseFloat(incomeInputText.value);
+        const savingInputText = document.getElementById('saving-percent');
+        const savingAmount = parseFloat(savingInputText.value);
+        let savingAmountTotal = (incomeAmount * savingAmount) / 100;
+        let savingTotal = document.getElementById('saving-total');
+        savingTotal.innerText = savingAmountTotal;
+
     })
