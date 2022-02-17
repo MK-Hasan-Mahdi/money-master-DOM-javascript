e2c4797fe52155a31480f8a00976e463118febac
mk-hasan <mkhasan6891@gmail.com>
2022-02-17 17:57:33 +0600

    error handle add

--- js/script.js
+++ js/script.js
@@ -15,29 +15,42 @@ function getInputValue(inputId) {
 
 document.getElementById('calcu-btn').addEventListener
     ('click', function () {
+        // income
+        const incomeAmountValue = getInputValue('income-field', false);
         // cost calculation
         const foodCostValue = getInputValue('food-cost', false);
         const rentCostValue = getInputValue('rent-cost', false);
         const clothCostValue = getInputValue('cloth-cost', false);
         // total exp
         const totalExp = document.getElementById('total-exp');
-        totalExp.innerText = foodCostValue + rentCostValue + clothCostValue;
+        const totalExpenseAmount = foodCostValue + rentCostValue + clothCostValue;
+        totalExp.innerText = totalExpenseAmount;
 
-        // income
-        const incomeAmountValue = getInputValue('income-field', false)
+
         // balance after expenses
         const balanceTotal = document.getElementById('balance-after-exp');
-        const balanceAmount = balanceTotal.innerText;
-        balanceTotal.innerText = incomeAmountValue - totalExp.innerText;
+        balanceTotal.innerText = incomeAmountValue - totalExpenseAmount;
+
+        const balanceAmount = parseFloat(balanceTotal.innerText);
+        if (balanceAmount < 0) {
+            alert('cant negative value');
+        }
     })
+
+
 
 document.getElementById('save-btn').addEventListener
     ('click', function () {
-        let incomeAmountValue = getInputValue('income-field');
-        const savingAmountValue = getInputValue('saving-percent');
+        const totalExp = document.getElementById('total-exp').innerText;
+        const balanceAfterExp = document.getElementById('balance-after-exp').innerText;
+        const incomeAmountValue = parseFloat(totalExp) + parseFloat(balanceAfterExp);
+        let savingAmountValue = document.getElementById('saving-percent').value;
         let savingAmountTotal = (incomeAmountValue * savingAmountValue) / 100;
+        // console.log(savingAmountTotal);
         let savingTotal = document.getElementById('saving-total');
         savingTotal.innerText = savingAmountTotal;
-        // remaining balance
 
+        // remaining balance
+        let remainBalance = document.getElementById('balance-remaining');
+        remainBalance.innerText = balanceAfterExp - savingAmountTotal;
     })
